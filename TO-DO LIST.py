f108aab3af7f86f843b5bc93a45f1131a04f2b12
unknown <bscs22f13@namal.edu.pk>
2025-03-04 01:14:06 -0800

    file-saving-and-loading

--- TO-DO LIST.py
+++ TO-DO LIST.py
@@ -1,19 +1,8 @@
-from tkinter import *
 
-def add_task():
-    task = entry.get()
-    if task != "":
-        listbox.insert(END, task)
-        entry.delete(0, END)
+def save_tasks():
+    with open("tasks.txt", "w") as file:
+        tasks = listbox.get(0, END)
+        for task in tasks:
+            file.write(task + "\n")
 
-root = Tk()
-root.title("To-Do List")
 
-entry = Entry(root, width=30)
-entry.pack()
-
-Button(root, text="Add Task", command=add_task).pack()
-listbox = Listbox(root)
-listbox.pack()
-
-root.mainloop()
