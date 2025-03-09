4d06df3894d432f3c84730e726159bcc6effad3b
Ahmad307777 <164899425+Ahmad307777@users.noreply.github.com>
2025-03-09 13:02:00 +0500

    TO-DO LIST.py

--- TO-DO LIST.py
+++ TO-DO LIST.py
@@ -1,8 +1,22 @@
+from tkinter import *
 
-def save_tasks():
-    with open("tasks.txt", "w") as file:
-        tasks = listbox.get(0, END)
-        for task in tasks:
-            file.write(task + "\n")
+def add_task():
+    task = task_entry.get()
+    if task != "":
+        listbox.insert(END, task)
+        task_entry.delete(0, END)
+
+root = Tk()
+root.title("To-Do List")
+
+task_entry = Entry(root, width=30)
+task_entry.pack()
+
+Button(root, text="Add Task", command=add_task).pack()
+listbox = Listbox(root)
+listbox.pack()
+
+root.mainloop()
+
 
 
